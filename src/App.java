f2a756b48722e407a96d19da4f04ab4ebae7081d
Renato Alvim <renatoalvim666@gmail.com>
2021-04-01 20:52:38 -0300

    Mudei a Classe seu Babaca

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        System.out.println("Alterei o projeto seu babaca do futuro :DDDDD");
+
+        Classe renato = new Classe(22);
+
+        System.out.println(renato.getBabaca());
     }
 }
---
+++ src/Classe.java
@@ -0,0 +1,23 @@
+public class Classe{
+
+    private int babaca;
+
+    public Classe(int babaca) {
+        this.babaca = babaca;
+    }
+
+    public int getBabaca() {
+        return babaca;
+    }
+
+    public void setBabaca(int babaca) {
+        this.babaca = babaca;
+    }
+
+    @Override
+    public String toString() {
+        return "Classe [babaca=" + babaca + "]";
+    }
+    
+
+}
